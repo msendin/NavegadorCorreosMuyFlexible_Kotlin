5c25b81b930b93a883de171218a9270f1b77035e
Montse <msendinv@gmail.com>
2022-05-08 22:30:07 +0200

    viewBinding in MainActivity

--- app/src/main/java/udl/eps/demos/navegadorcorreosmuyflexible_kotlin/MainActivity.kt
+++ app/src/main/java/udl/eps/demos/navegadorcorreosmuyflexible_kotlin/MainActivity.kt
@@ -3,13 +3,16 @@ package udl.eps.demos.navegadorcorreosmuyflexible_kotlin
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import udl.eps.demos.navegadorcorreosmuyflexible_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : FragmentActivity(), FragmentListado.CorreosListener {
     val EXTRA_TEXTO = "cat.udl.eps.fragments.ejmoreflexible.EXTRA_TEXTO"
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val frgListado = supportFragmentManager
             .findFragmentById(R.id.FrgListado) as FragmentListado?
         frgListado?.setCorreosListener(this)
